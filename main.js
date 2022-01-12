6f7fa84c2ab8c03725b13c76b6937984c1b9a9d8
TurgutHarunArslan <nazancakar623@gmail.com>
2022-01-13 00:27:38 +0300

    Js uptdates

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ function Myfuc() {
     var x = document.getElementById("acc");
     if (x.style.display === "none") {
       x.style.display = "block";
+      document.getElementById("Dn").innerHTML = "Kapat";
     } else {
       x.style.display = "none";
+      document.getElementById("Dn").innerHTML = "5/A Sınıfı";
     }
   }
